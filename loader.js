357135ffdd58934228922bd17665f5a7ba472da9
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 16:00:50 -0400

    将寒假班级旅行移至开学前一周

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917195752';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917200030';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917195752';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917195752';
+} from './overlay-position.js?build=20260917200030';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917200030';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
