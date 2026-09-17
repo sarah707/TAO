3304c03775e94f7798da1d283856d953b92eae43
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 13:40:18 -0400

    Update invitation failure message

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917173748';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917174000';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917173748';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917173748';
+} from './overlay-position.js?build=20260917174000';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917174000';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
