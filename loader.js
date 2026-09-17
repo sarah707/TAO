fc03c03bc17c1a8123cad1527d3d88086f69c6b9
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 16:09:05 -0400

    将单科奖学金调整为五千元

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917200605';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917200841';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917200605';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917200605';
+} from './overlay-position.js?build=20260917200841';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917200841';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
