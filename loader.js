5b650a0be6d8e092cbcdf6c41263b7a52822fcb9
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 13:35:45 -0400

    Add display regex for game data tags

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917172729';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917173526';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917172729';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917172729';
+} from './overlay-position.js?build=20260917173526';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917173526';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
