b25b952e826e3a403a9b9673d7c7221ebe1b5e5f
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 17:26:30 -0400

    Fix chapter reading position during background rerenders

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917210432';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917212527';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917210432';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917210432';
+} from './overlay-position.js?build=20260917212527';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917212527';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
