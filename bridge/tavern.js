7a6059a61666eea1ac37377e8ca7fbcafc6c6e15
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 17:32:17 -0400

    Fix missing homework tasks and save final completion

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,5 +1,5 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260917212527';
-import { buildExportWorldbook } from './worldbook.js?build=20260917212527';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260917213143';
+import { buildExportWorldbook } from './worldbook.js?build=20260917213143';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
