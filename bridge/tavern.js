64c3c58dfd47db2d8e6441e58573bc6d917708a1
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 13:24:43 -0400

    移除实习邀约空状态重复文案

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,10 +1,10 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260917172012';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260917172401';
 import {
   buildGraduationWorldbook,
   buildLiveWorldbookName,
   buildLiveWorldbookPromptContext,
   mergeLiveWorldbookEntries
-} from './worldbook.js?build=20260917172012';
+} from './worldbook.js?build=20260917172401';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917172012';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260917172401';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260917172012';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917172012';
+} from './overlay-position.js?build=20260917172401';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260917172401';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
