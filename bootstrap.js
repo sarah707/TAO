f613d014220e819f0c458d719d5abf58d0855a99
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 12:14:31 -0400

    Use nearest matching tags for generated output

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
 const BOOTSTRAP_STATE_KEY = '__NOBLE_SCHOOL_REMOTE_BOOTSTRAP_V1__';
 const VERSION_MANIFEST_URL = 'https://sarah707.github.io/TAO/version.json';
-const FALLBACK_LOADER_URL = 'https://sarah707.github.io/TAO/loader.js?build=20260917160510';
+const FALLBACK_LOADER_URL = 'https://sarah707.github.io/TAO/loader.js?build=20260917161408';
 
 async function resolveLoaderUrl() {
   try {
