95a20f0d900c15aa5d5c39d24946fec22e3b8e31
HydrozoaWorks <kinr707@hotmail.com>
2026-09-18 02:02:00 -0400

    Restore filtered chapter examples with explicit protocol tags

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,5 +1,5 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260918055541';
-import { buildExportWorldbook } from './worldbook.js?build=20260918055541';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260918060108';
+import { buildExportWorldbook } from './worldbook.js?build=20260918060108';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
