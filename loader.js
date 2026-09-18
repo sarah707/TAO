92eee16645ebafeb8244ccc3d1e6f39e2590f5bc
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 21:14:14 -0400

    提高图书馆偶遇事件触发率

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918010434';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918011342';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260918010434';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918010434';
+} from './overlay-position.js?build=20260918011342';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918011342';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
