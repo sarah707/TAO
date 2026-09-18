203ce0fb8be355ae3b84a24808e9ff366d5d958b
HydrozoaWorks <kinr707@hotmail.com>
2026-09-18 02:31:32 -0400

    降低图书馆陌生人重复触发率

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918060803';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918063058';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260918060803';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918060803';
+} from './overlay-position.js?build=20260918063058';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918063058';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
