70d9c9fe6d95fe8fcca73deb0f780d9cfcf76e58
HydrozoaWorks <kinr707@hotmail.com>
2026-09-17 21:04:34 -0400

    Update GitHub test build 2026-09-17_21-04-34

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,5 +1,5 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260918010026';
-import { buildExportWorldbook } from './worldbook.js?build=20260918010026';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260918010434';
+import { buildExportWorldbook } from './worldbook.js?build=20260918010434';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918010026';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260918010434';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260918010026';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918010026';
+} from './overlay-position.js?build=20260918010434';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260918010434';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
