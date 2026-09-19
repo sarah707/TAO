ca0235188fd40eabc0f2237f835eeb9b7413722d
HydrozoaWorks <kinr707@hotmail.com>
2026-09-19 15:49:36 -0400

    Update GitHub test build 2026-09-19_15-48-26

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,6 +1,6 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260919194649';
-import { buildExportWorldbook } from './worldbook.js?build=20260919194649';
-import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260919194649';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260919194826';
+import { buildExportWorldbook } from './worldbook.js?build=20260919194826';
+import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260919194826';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
