e7b7f6bcdb6faadc32124ff5d8f97285cafb1a22
HydrozoaWorks <kinr707@hotmail.com>
2026-09-19 14:34:56 -0400

    Add post-dance lover reunion

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260919183004';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260919183435';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260919183004';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260919183004';
+} from './overlay-position.js?build=20260919183435';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260919183435';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
