54c44dbc2aff8bdb9217f5a85a4e7deb664cf540
HydrozoaWorks <kinr707@hotmail.com>
2026-09-20 13:41:46 -0400

    Update GitHub test build 2026-09-20_13-41-46

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260920171730';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260920174145';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260920171730';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260920171730';
+} from './overlay-position.js?build=20260920174145';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260920174145';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
