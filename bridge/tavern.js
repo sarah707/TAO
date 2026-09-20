20dc4495d940c53419915caf776b0b10a9aaaab9
HydrozoaWorks <kinr707@hotmail.com>
2026-09-20 12:30:31 -0400

    Fix character page responsive width

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,6 +1,6 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260920161249';
-import { buildExportWorldbook } from './worldbook.js?build=20260920161249';
-import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260920161249';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260920162927';
+import { buildExportWorldbook } from './worldbook.js?build=20260920162927';
+import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260920162927';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
