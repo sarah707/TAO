369df6f1664bc59d4e9254a601c54b62ee5fcec4
HydrozoaWorks <kinr707@hotmail.com>
2026-09-20 11:26:45 -0400

    Fix graduation action token handling

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260920150508';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260920152507';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260920150508';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260920150508';
+} from './overlay-position.js?build=20260920152507';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260920152507';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
