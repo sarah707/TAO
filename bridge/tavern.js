b88421202bc31cb5828e3a6ce1cb249f00bcc917
HydrozoaWorks <kinr707@hotmail.com>
2026-09-20 22:03:03 -0400

    Update GitHub test build 2026-09-20_22-03-03

--- bridge/tavern.js
+++ bridge/tavern.js
@@ -1,6 +1,6 @@
-import { makeGenerationId, sanitizeAiText } from './text.js?build=20260920235857';
-import { buildExportWorldbook } from './worldbook.js?build=20260920235857';
-import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260920235857';
+import { makeGenerationId, sanitizeAiText } from './text.js?build=20260921020303';
+import { buildExportWorldbook } from './worldbook.js?build=20260921020303';
+import { BUILTIN_PRESET_SETTINGS, cloneBuiltInRequestPreset } from './builtin-preset.js?build=20260921020303';
 
 export const BRIDGE_KEY = '__NOBLE_SCHOOL_TAVERN_BRIDGE_V1__';
 export const CHAT_STORAGE_VARIABLE = '$nobleSchoolGameStorage';
--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260920235857';
+import { BRIDGE_KEY, createTavernBridge } from './bridge/tavern.js?build=20260921020303';
 import {
   clampFloatingPosition,
   getDefaultMinimizedPosition,
   getVisibleViewportBounds
-} from './overlay-position.js?build=20260920235857';
-import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260920235857';
+} from './overlay-position.js?build=20260921020303';
+import { getActiveChatSnapshot, subscribeToChatChanges } from './chat-lifecycle.js?build=20260921020303';
 
 const OVERLAY_ID = 'noble-school-overlay';
 const STYLE_ID = 'noble-school-overlay-style';
